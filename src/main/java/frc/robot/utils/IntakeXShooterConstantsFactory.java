a3bb53714b401ef77b4c169747a1211af40ee32d
Yehooda Romem <yrrad8@gmail.com>
2026-01-27 11:09:13 +0200

    added constants factory for IntakeXShooter

---
+++ src/main/java/frc/robot/utils/IntakeXShooterConstantsFactory.java
@@ -0,0 +1,9 @@
+package frc.robot.utils;
+
+import frc.robot.subsystems.intakexshooter.IntakeXShooterConstants;
+
+public class IntakeXShooterConstantsFactory {
+    public static IntakeXShooterConstants createIntakeXShooterConstants() {
+        return new IntakeXShooterConstants("Robot/IntakeXShooter");
+    }
+}
