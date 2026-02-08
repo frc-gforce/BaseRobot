38050ccb252186546c6d56819ce0abc109fd8395
itamaroren8 <itamarorenstei@gamil.com>
2026-02-08 20:32:50 +0200

    Implemented Logitech controller support.

---
+++ src/main/java/frc/robot/controller/LogitechCommandController.java
@@ -0,0 +1,12 @@
+package frc.robot.controller;
+
+public class LogitechCommandController extends XboxCommandController{
+    /**
+     * Construct an instance of a device.
+     *
+     * @param port The port index on the Driver Station that the device is plugged into.
+     */
+    public LogitechCommandController(int port) {
+        super(port);
+    }
+}
