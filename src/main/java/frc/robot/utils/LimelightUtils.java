1d71bc3e9ddc4340768b239bdc6797ca9357435e
itamaroren8 <itamarorenstei@gamil.com>
2026-02-10 17:51:42 +0200

    Revert "Added pipeline usage for ball detection mode and tag detection mode, and updated all functions that require it to check the current mode, as well as a getter and setter for current mode (of type enum VisionMode)."
    This reverts commit 8364d4281efc2d91fbee8bc05b718397defcf8b0.

--- src/main/java/frc/robot/utils/LimelightUtils.java
+++ src/main/java/frc/robot/utils/LimelightUtils.java
@@ -6,33 +6,6 @@ import frc.robot.limelight.LimelightHelpers;
 
 public class LimelightUtils {
     public static final String NAME = "limelight";
-    public enum VisionMode {APRILTAG, BALL}
-    static private VisionMode currentMode;
-
-    /**
-     * Sets the limelight pipeline to the wanted target detection.
-     * @param mode Which target to detect. VisionMode.APRILTAG for tags (location), VisionMode.BALL for ball detection
-     */
-    public static void setVisionMode(VisionMode mode) {
-        switch (mode) {
-            case APRILTAG:
-                LimelightHelpers.setPipelineIndex(NAME, 0);
-                currentMode = VisionMode.APRILTAG;
-                break;
-            case BALL:
-                LimelightHelpers.setPipelineIndex(NAME, 1);
-                currentMode = VisionMode.BALL;
-                break;
-        }
-    }
-
-    /**
-     * Returns the current detection mode.
-     * @return VisionMode.APRILTAG or VisionMode.BALL for tags or ball detection
-     */
-    public static VisionMode getCurrentMode() {
-        return currentMode;
-    }
 
     /**
      * Checks if the robot currently has a target
@@ -46,30 +19,27 @@ public class LimelightUtils {
     /**
      * Returns the robot's 2d position in the field
      *
-     * @return The robot's 2d position if in tags mode, otherwise null
+     * @return The robot's 2d position
      */
     public static Pose2d getPose2d() {
-        if (currentMode != VisionMode.APRILTAG) return null;
         return LimelightHelpers.getBotPose2d_wpiBlue(NAME);
     }
 
     /**
      * Returns the robot's 3d position in the field
      *
-     * @return The robot's 3d position if in tags mode, otherwise null
+     * @return The robot's 3d position
      */
     public static Pose3d getPose3d() {
-        if (currentMode != VisionMode.APRILTAG) return null;
         return LimelightHelpers.getBotPose3d_wpiBlue(NAME);
     }
 
     /**
      * Returns the amount of tags in sight
      *
-     * @return Amount of tags detected in sight if in tags mode, otherwise -1
+     * @return Amount of tags detected in sight
      */
     public static int getTagCount() {
-        if (currentMode != VisionMode.APRILTAG) return -1;
         return LimelightHelpers.getBotPoseEstimate_wpiBlue(NAME).tagCount;
     }
 
@@ -78,7 +48,7 @@ public class LimelightUtils {
      * @return Latency from camera in ms (milliseconds)
      */
     public static double getLatency() {
-        return LimelightHelpers.getLatency_Capture(NAME)
-                + LimelightHelpers.getLatency_Pipeline(NAME);
+        return LimelightHelpers.getLatency_Capture("limelight")
+                + LimelightHelpers.getLatency_Pipeline("limelight");
     }
 }
