6698fbb620fceb780e74b708a15effd3241dbf51
Yehooda Romem <yrrad8@gmail.com>
2026-03-16 15:31:59 +0200

    whole autonomous with some testing

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -5,23 +5,124 @@
 
 package frc.robot.commands;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 
 
-
-public final class Autos
-{
-    /** Example static factory for an autonomous command. */
-    public static Command exampleAuto(ExampleSubsystem subsystem)
-    {
+public final class Autos {
+    /**
+     * Example static factory for an autonomous command.
+     */
+    public static Command exampleAuto(ExampleSubsystem subsystem) {
         return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
     }
-    
-    
-    private Autos()
-    {
+
+    public static Command testAuto() {
+        PathPlannerPath path = null;
+        try {
+            path = PathPlannerPath.fromPathFile("Test Path");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return AutoBuilder.followPath(path);
+    }
+
+    public static Command bottomRampAuto(Command shootCommand, Command intakeCommand, Command stopCommand) {
+        PathPlannerPath path1 = null;
+        PathPlannerPath path2 = null;
+        try {
+            path1 = PathPlannerPath.fromPathFile("bottom-ramp to middle");
+            //TODO create a new path for shooting pose
+            path2 = PathPlannerPath.fromPathFile("bottom-ramp to shoot");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //parallel command
+        return new SequentialCommandGroup(
+                //shootCommand
+                new ParallelRaceGroup(
+                        AutoBuilder.followPath(path1),
+                        //Open intake
+                        intakeCommand.asProxy()
+                ),
+                AutoBuilder.followPath(path2),
+                stopCommand,
+                shootCommand.asProxy()
+        );
+    }
+
+    public static Command bottomTrenchAuto(Command shootCommand, Command intakeCommand) {
+        PathPlannerPath path1 = null;
+        PathPlannerPath path2 = null;
+        try {
+            path1 = PathPlannerPath.fromPathFile("bottom-trench to middle");
+            //TODO create a new path for shooting pose
+            path2 = PathPlannerPath.fromPathFile("bottom-trench to shoot");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //parallel command
+        return new SequentialCommandGroup(
+                //shootCommand
+                new ParallelRaceGroup(
+                        AutoBuilder.followPath(path1),
+                        //Open intake
+                        intakeCommand.asProxy()
+                ),
+                AutoBuilder.followPath(path2),
+                shootCommand.asProxy()
+        );
+    }
+
+    public static Command upperRampAuto(Command shootCommand, Command intakeCommand) {
+        PathPlannerPath path1 = null;
+        PathPlannerPath path2 = null;
+        try {
+            path1 = PathPlannerPath.fromPathFile("bottom-ramp to middle").mirrorPath();
+            //TODO create a new path for shooting pose
+            path2 = PathPlannerPath.fromPathFile("bottom-ramp to shoot").mirrorPath();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //parallel command
+        return new SequentialCommandGroup(
+                //shootCommand
+                new ParallelRaceGroup(
+                        AutoBuilder.followPath(path1),
+                        //Open intake
+                        intakeCommand.asProxy()
+                ),
+                AutoBuilder.followPath(path2),
+                shootCommand.asProxy()
+        );
+    }
+
+    public static Command upperTrenchAuto(Command shootCommand, Command intakeCommand) {
+        PathPlannerPath path1 = null;
+        PathPlannerPath path2 = null;
+        try {
+            path1 = PathPlannerPath.fromPathFile("bottom-trench to middle").mirrorPath();
+            //TODO create a new path for shooting pose
+            path2 = PathPlannerPath.fromPathFile("bottom-trench to shoot").mirrorPath();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //parallel command
+        return new SequentialCommandGroup(
+                //shootCommand
+                new ParallelRaceGroup(
+                        AutoBuilder.followPath(path1),
+                        //Open intake
+                        intakeCommand.asProxy()
+                ),
+                AutoBuilder.followPath(path2),
+                shootCommand.asProxy()
+        );
+    }
+
+    private Autos() {
         throw new UnsupportedOperationException("This is a utility class!");
     }
 }
